0f9332650b1ba33470b10929d6edc0de06fb31a0
hyeju <hae7077@naver.com>
2021-06-22 13:18:13 +0900

    Initial commit

---
+++ src/main/java/solux/woodong/WoodongApplication.java
@@ -0,0 +1,13 @@
+package solux.woodong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoodongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoodongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/solux/woodong/WoodongApplicationTests.java
@@ -0,0 +1,13 @@
+package solux.woodong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoodongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
